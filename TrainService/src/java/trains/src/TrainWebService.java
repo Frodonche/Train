d942042aaead15a335c0f07e836fb00dfd93ddf9
FrLallemand <frlallemand@protonmail.com>
2018-12-13 11:39:18 +0100

    ajout du train par SOAP

--- TrainService/src/java/trains/src/TrainWebService.java
+++ TrainService/src/java/trains/src/TrainWebService.java
@@ -5,6 +5,7 @@
  */
 package trains.src;
 
+import java.util.Date;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -22,5 +23,19 @@ public class TrainWebService {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "addTrain")
+    public String addTrain(@WebParam(name = "identifiant") int identifiant, @WebParam(name = "villeDepart") String villeDepart, @WebParam(name = "villeArrivee") String villeArrivee, @WebParam(name = "dateDepart") Date dateDepart, @WebParam(name = "heureDepart") int heureDepart, @WebParam(name = "prixBillet") int prixBillet, @WebParam(name = "places") int places) {
+        Modele m = Modele.getInstance();
+        if(dateDepart.before(new Date())){
+            return "dateDepart must be later than current Date";
+        } else {
+            m.addTrain(identifiant, villeDepart, villeArrivee, dateDepart, heureDepart, prixBillet, places);
+            return "Ok";
+        }
     }
 }
