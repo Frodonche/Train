3ea7d8d3cdef4017e9aa91ea3c1482cd77a9f407
Steve <stevedbz@hotmail.fr>
2018-12-13 11:52:49 +0100

    ajout sql request

--- TrainService/src/java/trains/src/Modele.java
+++ TrainService/src/java/trains/src/Modele.java
@@ -7,6 +7,7 @@ package trains.src;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -22,6 +23,59 @@ public class Modele {
     
     public static Modele getInstance(){
         return INSTANCE;
+    }
+    
+    public String createSQLSelect(List<String> select, String from, List<String> where){
+        String ret="";
+        ret = "select ";
+        int i = 0;
+        while(i<select.size()){
+            ret = ret + select.get(i);
+            i++;
+            if(i<select.size())ret = ret + ", ";
+        }
+        ret = ret + " from "+from;
+        i = 0;
+        if (where.size()>0)ret = ret + " where ";
+        while(i<where.size()){
+            ret = ret + where.get(i)+ " = " +where.get(i);
+            i++;
+            if(i<where.size())ret = ret + " and ";
+        }
+        return ret;
+    }
+    
+    public String createSQLUpdate(List<String> set, String from, List<String> where){
+        String ret="";
+        ret = "update " + from;
+        int i = 0;
+        if(set.size()>0)ret = ret + " set ";
+        while(i<set.size()){
+            ret = ret + set.get(i) + " = " + set.get(i);
+            i++;
+            if(i<set.size())ret = ret + ", ";
+        }
+        i = 0;
+        if (where.size()>0)ret = ret + " where ";
+        while(i<where.size()){
+            ret = ret + where.get(i)+ " = " +where.get(i);
+            i++;
+            if(i<where.size())ret = ret + " and ";
+        }
+        return ret;
+    }
+    
+        public String createSQLDelete(String from, List<String> where){
+        String ret="";
+        ret = "delete from "+ from+" ";
+        int i = 0;
+        if (where.size()>0)ret = ret + " where ";
+        while(i<where.size()){
+            ret = ret + where.get(i)+ " = " +where.get(i);
+            i++;
+            if(i<where.size())ret = ret + " and ";
+        }
+        return ret;
     }
     
     public void addTrain(int identifiant, String villeDepart, String villeArrivee, Date dateDepart, int heureDepart, int prixBillet, int places){
